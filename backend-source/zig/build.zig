a96e6dd130229307f29b4d3aa79b34a3ffa67c6d
Martin <buehlemj@hotmail.com>
2025-07-13 11:43:00 +0200

    Nats removed

--- backend-source/zig/build.zig
+++ backend-source/zig/build.zig
@@ -5,10 +5,6 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("lmdbModule.zig"),
     });
 
-    // const natsModule = b.createModule(.{
-    //     .root_source_file = b.path("natsModule.zig"),
-    // });
-
     const exe = b.addExecutable(.{
         .name = "Main",
         .root_source_file = b.path("Main.zig"),
@@ -16,15 +12,8 @@ pub fn build(b: *std.Build) void {
     });
 
     exe.root_module.addImport("lmdbModule", lmdbModule);
-    // exe.root_module.addImport("natsModule", natsModule);
-    // natsModule.addImport("lmdbModule", lmdbModule);
-    // exe.addLibraryPath(.{ .cwd_relative = "/opt/vcpkg/installed/x64-linux/lib" });
     exe.linkSystemLibrary("c");
     exe.linkSystemLibrary("lmdb");
-    // exe.linkSystemLibrary("nats_static");
-    // exe.linkSystemLibrary("ssl");
-    // exe.linkSystemLibrary("crypto");
-    // exe.linkSystemLibrary("sodium");
 
     b.installArtifact(exe);
 }
